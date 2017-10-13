0d51bb45f7f4159a6c2be3eec811df946ffeea35
greggawatt <greggahorton@gmail.com>
2017-10-13 08:10:12 -0700

    1.5

---
+++ 1_4/HotCold/src/HotCold.java
@@ -0,0 +1,16 @@
+//1.4.34 Hot or cold. Your goal is to guess a secret integer between 1 and N. 
+//You repeatedly guess integers between 1 and N. After each guess you learn 
+//if your guess equals the secret integer (and the game stops). Otherwise, you 
+//learn if the guess is hotter (closer to) or colder (farther from) the secret 
+//number than your previous guess. Design an algorithm that finds the secret 
+//number in at most ~2 lg N guesses. Then design an algorithm that finds the 
+//secret number in at most ~ 1 lg N guesses.
+
+public class HotCold {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
